5b44e0693e2a37d6e7f7f31fcac133de11681698
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-16 18:38:47 +0200

    Import del router con le rotte (CRUD) nell'app.js + prove Postman - Esercizio base completato

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express"); /**Importiamo il framework "Express"*/
 const app = express(); /**Creiamo un'istanza dell'applicazione "Express"*/
 const port = 3000; /**Dichiariamo il numero della porta su cui il server ascolterà*/
-const posts = require("./routers/posts.js"); /**Import del modulo contenente l'array nel file "post.js"*/
+const postsRouter = require("./routers/posts.js"); /**Import del router con le rotte (CRUD) per i post*/
 
-app.use(express.static("public")); /**Configuriamo la cartella "public" per rendere siponibili i file "statici" (le immagini)*/
+app.use(express.static("public")); /**Middleware - Configuriamo la cartella "public" per rendere siponibili i file "statici" (le immagini)*/
+
+app.use(express.json()); /**Middleware - Configuriamo il file per leggere i dati JSON (for POST & PUT)*/
 
 app.get("/", (req, res) => { /**Definiamo la rotta principale ("/")*/
     res.send("<h1>Server del mio blog</h1>") /**Risposta inviata al client quando visita la home*/
 }); 
 
-app.get("/bacheca", (req, res) => { /**Definiamo la rotta per la bacheca*/
-    res.json({posts}); /**Restituzione dell'oggetto json*/
-}); 
+app.use("/posts", postsRouter); /**Indichiamo che tutte le rotte definite in "posts.js" partiranno con /posts*/
 
 app.listen(port, () => { /**Avviamo il server e lo facciamo "ascoltare" sulla porta indicata precedentemente*/
     console.log(`Prova di ricezione ${port}`); /**Console.log di prova per vedere se arriva la "chiamata"*/
