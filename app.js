b686373ee0032449d89955a508aed2f5962aff18
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-16 16:49:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express"); /**Importiamo il framework "Express"*/
+const app = express(); /**Creiamo un'istanza dell'applicazione "Express"*/
+const port = 3000; /**Dichiariamo il numero della porta su cui il server ascolterà*/
+const posts = require("./posts.js"); /**Import del modulo contenente l'array nel file "post.js"*/
+
+app.use(express.static("public")); /**Configuriamo la cartella "public" per rendere siponibili i file "statici" (le immagini)*/
+
+app.get("/", (req, res) => { /**Definiamo la rotta principale ("/")*/
+    res.send("<h1>Server del mio blog</h1>") /**Risposta inviata al client quando visita la home*/
+}); 
+
+app.get("/bacheca", (req, res) => { /**Definiamo la rotta per la bacheca*/
+    res.json({posts}); /**Restituzione dell'oggetto json*/
+}); 
+
+app.listen(port, () => { /**Avviamo il server e lo facciamo "ascoltare" sulla porta indicata precedentemente*/
+    console.log(`Prova di ricezione ${port}`); /**Console.log di prova per vedere se arriva la "chiamata"*/
+});
